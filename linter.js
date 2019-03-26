bcdcfdba751b7d92c9d902a31d9d4c5d00191e48
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-03-26 01:20:40 -0400

    add summary in console.log output

--- linter.js
+++ linter.js
@@ -3,19 +3,22 @@
 // Enter your desired settings here:
 var settings = [
     {
-        s: 'h1', // selector
-        p: 'color', // property
-        v: 'red' // expected value
+        s:'h1', // selector
+        p:'color', // property
+        v:'red' // expected value
     }
 ];
 
+var errors = [];
 for (var i=0; i<settings.length; i++) {
-    lint(settings[i]);
+    errors.push(lint(settings[i]));
 }
 
-alert('Finished linting. Hover over the buttons.');
+alert('Finished linting. Hover over the buttons.\n\nA summary is also listed in the console log.');
+console.log(errors);
 
 function lint(setting) {
+    var errorSummary = '';
     var selector = setting.s || setting.selector;
     var property = setting.p || setting.property;
     var expectedValue = setting.v || setting.ev || setting.value || setting.expected || setting.expectedValue;
@@ -43,8 +46,17 @@ function lint(setting) {
         var spn = document.createElement("SPAN");
         spn.style.cssText = 'all: initial; position: relative; width: 0; height: 0;';
         spn.appendChild(btn);
-        if (element) element.appendChild(spn);
+        if (element) {
+            element.appendChild(spn);
+            errorSummary = {
+                selector:selector,
+                property:property,
+                expectedValue:expectedValue,
+                actualValue:actualValue
+            };
+        }
     }
+    return errorSummary;
 }
 
 function settingPropertiesSet(selector, property, valueExpected) {
