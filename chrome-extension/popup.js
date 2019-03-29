1291c5a28a05c9b813e6d39b2080d49130eaa198
hchiam <hchiam@users.noreply.github.com>
2019-03-28 23:58:52 -0400

    specify variables in one place (close #6)

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,8 +1,13 @@
 'use strict';
 
 let settingsButton = document.getElementById('use-settings');
+let variablesTextarea = document.getElementById('set-variables');
 let settingsTextarea = document.getElementById('set-settings');
 let clearErrorButtonsButton = document.getElementById('clear-error-buttons');
+
+chrome.storage.sync.get('variables', function(data) {
+  variablesTextarea.value = data.variables.replace(/^\s+|\s+$/g, '') || '';
+});
 
 chrome.storage.sync.get('settings', function(data) {
 settingsTextarea.value = data.settings.replace(/^\s+|\s+$/g, '') || `// Enter your desired settings here:
@@ -15,14 +20,19 @@ var settings = [
 ];`;
 });
 
+variablesTextarea.onkeyup = function setVariables() {
+  chrome.storage.sync.set({variables: variablesTextarea.value}, function() {});
+};
+
 settingsTextarea.onkeyup = function setSettings() {
   chrome.storage.sync.set({settings: settingsTextarea.value}, function() {});
 };
 
 settingsButton.addEventListener("click", function useSettings() {
+  variablesTextarea.value = variablesTextarea.value.replace(/^\s+|\s+$/g, '');
   settingsTextarea.value = settingsTextarea.value.replace(/^\s+|\s+$/g, '');
-  if (!isValidInput(settingsTextarea.value)) {
-    alert(`Invalid input. Please enter something like this: 
+  if (!isValidSettingsInput(settingsTextarea.value)) {
+    alert(`Invalid input for settings. Please enter something like this: 
 
 // Enter your desired settings here:
 var settings = [
@@ -36,10 +46,14 @@ var settings = [
     return; // do not continue
   }
   chrome.tabs.executeScript(null, {
-    code: settingsTextarea.value
+    code: isValidVariablesInput(variablesTextarea.value)
   }, function() {
+    chrome.tabs.executeScript(null, {
+      code: settingsTextarea.value
+    }, function() {
       chrome.tabs.executeScript(null, {file: 'main.js'});
       window.close();
+    });
   });
 });
 
@@ -54,9 +68,20 @@ clearErrorButtonsButton.addEventListener("click", function useSettings() {
   });
 });
 
-function isValidInput(inputString) {
-  var isCommentAndArrayJSON = inputString.match(/^\/\/ Enter your desired settings here:\nvar settings = \[\n    \{/);
-  var isArrayJSON = inputString.match(/^var settings = \[\n    \{/);
-  var isEndingWithArrayJSON = inputString.match(/\];$/);
+function isValidVariablesInput(variablesSetupString) {
+  var isEachLineAVariableDeclarationOrComment = variablesSetupString.split('\n').every(function(line) {
+    return line.match(/((^var .+? = .+?;$)|(^\/\/.*$)|(^\s*$))/);
+  });
+  if (!isEachLineAVariableDeclarationOrComment) {
+    alert('Invalid input for variables.');
+    return ''; // don't use if invalid
+  }
+  return variablesSetupString;
+}
+
+function isValidSettingsInput(settingsInputString) {
+  var isCommentAndArrayJSON = settingsInputString.match(/^\/\/ Enter your desired settings here:\nvar settings = \[\n    \{/);
+  var isArrayJSON = settingsInputString.match(/^var settings = \[\n    \{/);
+  var isEndingWithArrayJSON = settingsInputString.match(/\];$/);
   return ((isCommentAndArrayJSON != null) || (isArrayJSON != null)) && (isEndingWithArrayJSON != null);
 }
