e8bb002d304fbd3dd9a958e4f4b303f51920ea22
hchiam <hchiam@users.noreply.github.com>
2019-03-28 23:16:26 -0400

    add some validation

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -5,10 +5,10 @@ let settingsTextarea = document.getElementById('set-settings');
 let clearErrorButtonsButton = document.getElementById('clear-error-buttons');
 
 chrome.storage.sync.get('settings', function(data) {
-settingsTextarea.value = data.settings || `// Enter your desired settings here:
+settingsTextarea.value = data.settings.replace(/^\s+|\s+$/g, '') || `// Enter your desired settings here:
 var settings = [
     {
-        s:'*', // selector
+        s:'h1', // selector
         p:'color', // property
         v:'red' // expected value
     }
@@ -20,6 +20,21 @@ settingsTextarea.onkeyup = function setSettings() {
 };
 
 settingsButton.addEventListener("click", function useSettings() {
+  settingsTextarea.value = settingsTextarea.value.replace(/^\s+|\s+$/g, '');
+  if (!isValidInput(settingsTextarea.value)) {
+    alert(`Invalid input. Please enter something like this: 
+
+// Enter your desired settings here:
+var settings = [
+    {
+        s:'h1', // selector
+        p:'color', // property
+        v:'red' // expected value
+    }
+];`);
+    settingsTextarea.focus();
+    return; // do not continue
+  }
   chrome.tabs.executeScript(null, {
     code: settingsTextarea.value
   }, function() {
@@ -38,3 +53,10 @@ clearErrorButtonsButton.addEventListener("click", function useSettings() {
     `
   });
 });
+
+function isValidInput(inputString) {
+  var isCommentAndArrayJSON = inputString.match(/^\/\/ Enter your desired settings here:\nvar settings = \[\n    \{/);
+  var isArrayJSON = inputString.match(/^var settings = \[\n    \{/);
+  var isEndingWithArrayJSON = inputString.match(/\];$/);
+  return ((isCommentAndArrayJSON != null) || (isArrayJSON != null)) && (isEndingWithArrayJSON != null);
+}
